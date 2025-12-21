2a9368d0bf664e2be5343ee4607dfd966c39db97
aabb <809073023@qq.com>
2025-12-21 20:18:43 +0800

    feat:add wallet privateKey and pubKey and pubKeyAddress

---
+++ 03-wallet/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var quickNodeURL = "https://magical-wild-model.ethereum-sepolia.quiknode.pro/4291e8c1bb9318f2307ff3ff5174826487ef3940/"
+var localURL = "http://localhost:8545"
+
+func main() {
+	// 生成私钥
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pData := crypto.FromECDSA(privateKey)
+	fmt.Printf("私钥:%v \n", hexutil.Encode(pData))
+	// 公钥由私钥生成
+	puData := crypto.FromECDSAPub(&privateKey.PublicKey)
+	fmt.Printf("公钥:%v \n", hexutil.Encode(puData))
+	address := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
+	fmt.Printf("公钥地址:%v \n", address)
+
+}
