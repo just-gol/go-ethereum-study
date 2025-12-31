5b2330b5ade4c3ca2869f049d2f9e43469929144
aabb <809073023@qq.com>
2025-12-31 21:13:57 +0800

    refactor: optimize code logic

--- 08-go-solidity-when/main.go
+++ 08-go-solidity-when/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 
+	"08-go-solidity-when/bootstrap"
 	"08-go-solidity-when/config"
-	"08-go-solidity-when/handle"
-	"08-go-solidity-when/routers"
-	"08-go-solidity-when/service"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -21,30 +13,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	client, err := ethclient.Dial(cfg.RPCURL)
+	r, err := bootstrap.NewApp(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	whenService := service.NewWhenService(client)
-	handler := handle.NewWhenHandler(whenService, cfg.ContractAddress)
-	txService := service.NewTxService(client)
-	txHandler := handle.NewTxHandler(txService)
-	approvalService := service.NewApprovalService()
-	approvalHandle := handle.NewApprovalHandle(approvalService)
-
-	wsClient, err := ethclient.Dial(cfg.WSURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	listenerSvc := service.NewListenerService(wsClient)
-	if cfg.ContractAddress != "" {
-		contractAddress := common.HexToAddress(cfg.ContractAddress)
-		go func() {
-			listenerSvc.MonitorEvent(context.Background(), contractAddress)
-		}()
-	}
-	r := gin.Default()
-	r.Use(cors.Default())
-	routers.ApiRoutersInit(r, handler, txHandler, approvalHandle)
 	_ = r.Run()
 }
